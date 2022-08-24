3480f97e606fa2310a731b8059acb4806c20ad61
chrisgabs <cgabayquen@gmail.com>
2022-08-25 00:37:05 +0800

    fix: fixed pieces placement logic

--- pages/tictactoe.tsx
+++ pages/tictactoe.tsx
@@ -2,11 +2,11 @@ import type { NextPage } from 'next'
 import { stringify } from 'querystring';
 import styles from '../styles/Tictactoe.module.sass'
 
-let dragged : any = null;
+let dragged : any = null
 
-const drag = (e: any) => {
-    e.dataTransfer.setData("text", e.target.id)
-    e.dataTransfer.setData("hatdog", e.target.id)
+const onDragStart = (e: any) => {
+    // e.dataTransfer.setData("text", e.target.id)
+    // e.dataTransfer.setData("hatdog", e.target.id)
     dragged = e.target
 }
 
@@ -14,51 +14,63 @@ const dragOver = (e: any) => {
     e.preventDefault()
 }
 
-const drop = (e: any) => {
-    console.log(e)
-    dragged.parentNode.removeChild(dragged);
-    e.target.appendChild(dragged)
+const onDragEnd = (e: any) => {
+    dragged = null
+}
+
+const drop = (e: any, accepts?:string) => {
+    // console.log(dragged)    
+    // remove when implementing kain mechanics
+    if (dragged == null || e.target.children.length > 0) 
+        return
+    if (accepts == undefined || accepts == dragged.id.substring(0,2)) {
+        dragged.parentNode.removeChild(dragged)
+        e.target.appendChild(dragged)
+        dragged.setAttribute("draggable", "return false")
+        dragged.setAttribute("onDragStart", "return false")
+    }
     console.log("dropped")
+}
+
+const createPieces = (player: number) => {
+    let pieces: any = []
+    for (let i = 0, size = 30; i < 5; i++, size += 5) {
+        let p = <div
+            id={"p" + player + "s" + i}
+            className={"piece p" + player}
+            draggable="true"
+            style={{ width: size + "px", height: size + "px" }}
+            onDragStart={onDragStart}
+            onDragEnd={onDragEnd}> </div>
+        pieces.push(p)
+    }
+    return pieces;
+}
+
+const createTiles = () => {
+    let rows: any = []
+    let tileNum = 0
+    for (let i = 0; i < 3; i++) {
+        let tiles = []
+        for (let i = 0; i < 3; i++, tileNum++) {
+            let tile = <div
+                className="tile"
+                id={"t" + tileNum}
+                onDragOver={dragOver}
+                onDrop={e => drop(e)}></div>
+            tiles.push(tile)
+        }
+        let row: any = <div className="row">{tiles}</div>
+        rows.push(row)
+    }
+    return rows;
 }
 
 const TicTacToe: NextPage = () => {
 
-    const createPieces = (player:number) => {
-        let pieces: any = []
-        for (let i = 0, size = 30; i < 5; i++, size+=5) {
-            let p = <div 
-                id={"p" + player +"s" + i} 
-                className={"piece p" + player} 
-                draggable="true" 
-                style={{ width: size + "px", height: size + "px" }}
-                onDrag={drag}> </div>
-            pieces.push(p)
-        }
-        return pieces;
-    }
-
-    const createTiles = () => {
-        let rows: any = []
-        let tileNum = 0
-        for (let i = 0; i < 3; i++) {
-            let tiles = []
-            for (let i = 0; i < 3; i++, tileNum++) {
-                let tile = <div 
-                className="tile" 
-                id={"t" + tileNum} 
-                onDragOver={dragOver} 
-                onDrop={drop}></div>
-                tiles.push(tile)
-            }
-            let row: any = <div className="row">{tiles}</div>
-            rows.push(row)
-        }
-        return rows;
-    }
-
     return (
         <div id="main-container">
-            <div className="pieces-container" onDrop={drop} onDragOver={dragOver}>
+            <div className="pieces-container">
                 {createPieces(1)}
             </div>
 
@@ -66,7 +78,7 @@ const TicTacToe: NextPage = () => {
                 {createTiles()}
             </div>
 
-            <div className="pieces-container" onDrop={drop} onDragOver={dragOver}>
+            <div className="pieces-container">
                 {createPieces(2)}
             </div>
         </div>
