99dd8cb080cafd349af04d1424c4e54c6234dd69
chrisgabs <cgabayquen@gmail.com>
2022-08-26 13:28:21 +0800

    fix: fixed dropping pieces on tiles and pieces logic

--- pages/tictactoe.tsx
+++ pages/tictactoe.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import { stringify } from 'querystring';
+import React from 'react';
 import styles from '../styles/Tictactoe.module.sass'
 
 let dragged : any = null
@@ -19,29 +20,39 @@ const onDragEnd = (e: any) => {
 }
 
 const drop = (e: any, accepts?:string) => {
-    // console.log(dragged)    
-    // remove when implementing kain mechanics
-    if (dragged == null || e.target.children.length > 0) 
-        return
-    if (accepts == undefined || accepts == dragged.id.substring(0,2)) {
-        dragged.parentNode.removeChild(dragged)
+    if (e.target.id.substring(0,1) == "p"){
+        if (dragged.id.substring(dragged.id.length -1) > e.target.id.substring(e.target.id.length -1)) {
+            let parent = e.target.parentNode
+            parent.removeChild(parent.children[0])
+            parent.appendChild(dragged)
+            dragged.setAttribute("draggable", "return false")
+            dragged.setAttribute("onDragStart", "return false")
+        }
+        return;
+    }else { // if dropped on tile
+        if (e.target.hasChildNodes()) {
+            if (dragged.id.substring(dragged.id.length - 1) <= e.target.children[0].id.substring(e.target.id.length - 1))
+                return
+            e.target.removeChild(e.target.children[0])
+        }
         e.target.appendChild(dragged)
         dragged.setAttribute("draggable", "return false")
         dragged.setAttribute("onDragStart", "return false")
     }
-    console.log("dropped")
 }
 
 const createPieces = (player: number) => {
     let pieces: any = []
     for (let i = 0, size = 30; i < 5; i++, size += 5) {
-        let p = <div
+        let p = <React.Fragment key={"p" + player + "s" + i} ><div
             id={"p" + player + "s" + i}
             className={"piece p" + player}
             draggable="true"
             style={{ width: size + "px", height: size + "px" }}
             onDragStart={onDragStart}
-            onDragEnd={onDragEnd}> </div>
+            onDragEnd={onDragEnd}
+            onDragOver={dragOver}
+            onDrop={drop}> </div> </React.Fragment>
         pieces.push(p)
     }
     return pieces;
@@ -53,14 +64,19 @@ const createTiles = () => {
     for (let i = 0; i < 3; i++) {
         let tiles = []
         for (let i = 0; i < 3; i++, tileNum++) {
-            let tile = <div
+            let tile = 
+            <React.Fragment key={"t" + tileNum} >
+                <div
                 className="tile"
                 id={"t" + tileNum}
                 onDragOver={dragOver}
-                onDrop={e => drop(e)}></div>
+                onDrop={e => drop(e)}>
+                </div>
+            </React.Fragment>
+
             tiles.push(tile)
         }
-        let row: any = <div className="row">{tiles}</div>
+        let row: any = <React.Fragment key={"r" + i}> <div className="row">{tiles} </div></React.Fragment>
         rows.push(row)
     }
     return rows;
