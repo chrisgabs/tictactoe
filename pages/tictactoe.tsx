adc233180d092bc1f306ca0b7fe667ccf7567947
chrisgabs <cgabayquen@gmail.com>
2022-08-28 16:29:35 +0800

    fix: fixed local variables to use state instead

--- pages/tictactoe.tsx
+++ pages/tictactoe.tsx
@@ -2,92 +2,98 @@ import type { NextPage } from 'next'
 import { stringify } from 'querystring';
 import React from 'react';
 import styles from '../styles/Tictactoe.module.sass'
+import { useEffect, useState, } from "react";
 
-let dragged : any = null
+const TicTacToe: NextPage = () => {
+    const [dragged, setDragged] = useState<any>(null);
+    const [playerNum, setPlayerNum] = useState<number>(1);
+    const [playerTurn, setPlayerTurn] = useState<number>(1);
 
-const onDragStart = (e: any) => {
-    // e.dataTransfer.setData("text", e.target.id)
-    // e.dataTransfer.setData("hatdog", e.target.id)
-    dragged = e.target
-}
+    useEffect(() => {
+        
+    }, [])
 
-const dragOver = (e: any) => {
-    e.preventDefault()
-}
+    const onDragStart = (e: any) => {
+        // e.dataTransfer.setData("text", e.target.id)
+        // e.dataTransfer.setData("hatdog", e.target.id)
+        setDragged(e.target)
+    }
 
-const onDragEnd = (e: any) => {
-    dragged = null
-}
+    const dragOver = (e: any) => {
+        e.preventDefault()
+    }
 
-const drop = (e: any, accepts?:string) => {
-    if (e.target.id.substring(0,1) == "p"){
-        if (dragged.id.substring(dragged.id.length -1) > e.target.id.substring(e.target.id.length -1)) {
-            let parent = e.target.parentNode
-            parent.removeChild(parent.children[0])
-            parent.appendChild(dragged)
+    const onDragEnd = (e: any) => {
+        setDragged(null)
+    }
+
+    const drop = (e: any, accepts?: string) => {
+        if (e.target.id.substring(0, 1) == "p") {
+            if (dragged.id.substring(dragged.id.length - 1) > e.target.id.substring(e.target.id.length - 1)) {
+                let parent = e.target.parentNode
+                parent.removeChild(parent.children[0])
+                parent.appendChild(dragged)
+                dragged.setAttribute("draggable", "return false")
+                dragged.setAttribute("onDragStart", "return false")
+            }
+            return;
+        } else { // if dropped on tile
+            if (e.target.hasChildNodes()) {
+                if (dragged.id.substring(dragged.id.length - 1) <= e.target.children[0].id.substring(e.target.id.length - 1))
+                    return
+                e.target.removeChild(e.target.children[0])
+            }
+            e.target.appendChild(dragged)
             dragged.setAttribute("draggable", "return false")
             dragged.setAttribute("onDragStart", "return false")
         }
-        return;
-    }else { // if dropped on tile
-        if (e.target.hasChildNodes()) {
-            if (dragged.id.substring(dragged.id.length - 1) <= e.target.children[0].id.substring(e.target.id.length - 1))
-                return
-            e.target.removeChild(e.target.children[0])
-        }
-        e.target.appendChild(dragged)
-        dragged.setAttribute("draggable", "return false")
-        dragged.setAttribute("onDragStart", "return false")
     }
-}
 
-const createPieces = (player: number) => {
-    let pieces: any = []
-    for (let i = 0, size = 30; i < 5; i++, size += 5) {
-        let p = <React.Fragment key={"p" + player + "s" + i} ><div
-            id={"p" + player + "s" + i}
-            className={"piece p" + player}
-            draggable="true"
-            style={{ width: size + "px", height: size + "px" }}
-            onDragStart={onDragStart}
-            onDragEnd={onDragEnd}
-            onDragOver={dragOver}
-            onDrop={drop}> </div> </React.Fragment>
-        pieces.push(p)
-    }
-    return pieces;
-}
-
-const createTiles = () => {
-    let rows: any = []
-    let tileNum = 0
-    for (let i = 0; i < 3; i++) {
-        let tiles = []
-        for (let i = 0; i < 3; i++, tileNum++) {
-            let tile = 
-            <React.Fragment key={"t" + tileNum} >
-                <div
-                className="tile"
-                id={"t" + tileNum}
+    const createPieces = (player: number) => {
+        let pieces: any = []
+        for (let i = 0, size = 30; i < 5; i++, size += 5) {
+            let p = <React.Fragment key={"p" + player + "s" + i} ><div
+                id={"p" + player + "s" + i}
+                className={"piece p" + player}
+                draggable="true"
+                style={{ width: size + "px", height: size + "px" }}
+                onDragStart={onDragStart}
+                onDragEnd={onDragEnd}
                 onDragOver={dragOver}
-                onDrop={e => drop(e)}>
-                </div>
-            </React.Fragment>
-
-            tiles.push(tile)
+                onDrop={drop}> </div> </React.Fragment>
+            pieces.push(p)
         }
-        let row: any = <React.Fragment key={"r" + i}> <div className="row">{tiles} </div></React.Fragment>
-        rows.push(row)
+        return pieces;
     }
-    return rows;
-}
 
-const TicTacToe: NextPage = () => {
+    const createTiles = () => {
+        let rows: any = []
+        let tileNum = 0
+        for (let i = 0; i < 3; i++) {
+            let tiles = []
+            for (let i = 0; i < 3; i++, tileNum++) {
+                let tile =
+                    <React.Fragment key={"t" + tileNum} >
+                        <div
+                            className="tile"
+                            id={"t" + tileNum}
+                            onDragOver={dragOver}
+                            onDrop={e => drop(e)}>
+                        </div>
+                    </React.Fragment>
+
+                tiles.push(tile)
+            }
+            let row: any = <React.Fragment key={"r" + i}> <div className="row">{tiles} </div></React.Fragment>
+            rows.push(row)
+        }
+        return rows;
+    }
 
     return (
         <div id="main-container">
             <div className="pieces-container">
-                {createPieces(1)}
+                {createPieces(playerNum == 1 ? 2 : 1)}
             </div>
 
             <div className="parent">
@@ -95,7 +101,7 @@ const TicTacToe: NextPage = () => {
             </div>
 
             <div className="pieces-container">
-                {createPieces(2)}
+                {createPieces(playerNum)}
             </div>
         </div>
     )
